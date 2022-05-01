e52c5dd3f3e12ec09500e676d41a63ba84039fd7
Arabe Khan <khan.arabe016@gmail.com>
2022-05-02 05:40:31 +0600

    first cit

--- custom.js
+++ custom.js
@@ -1,6 +1,6 @@
 let canvas = new fabric.Canvas("canvas", {
   width: 800,
-  height: 500,
+  height: 700,
   backgroundColor: "blue",
 });
 
@@ -12,25 +12,34 @@ document.getElementById("uploadImg").onchange = (e) => {
     image.onload = function () {
       var img = new fabric.Image(image);
       img.set({
-        left: 100,
-        top: 60,
+        left: canvas.width / 3,
+        top: canvas.height / 3,
       });
-      img.scaleToWidth(200);
+      img.scaleToWidth(300);
       canvas.add(img).setActiveObject(img).renderAll();
     };
+    canvas.on("mouse:wheel", function (opt) {
+      var delta = opt.e.deltaY;
+      var zoom = canvas.getZoom();
+      zoom *= 0.999 ** delta;
+      if (zoom > 20) zoom = 20;
+      if (zoom < 1) {
+        zoom = 1;
+        var img = canvas.getActiveObject();
+        canvas.remove(img);
+        canvas.centerObject(img);
+        canvas.setActiveObject(img);
+        canvas.add(img);
+        canvas.setViewportTransform([1, 0, 0, 1, 0, 0]);
+
+        canvas.renderAll();
+      }
+      canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
+      opt.e.preventDefault();
+      opt.e.stopPropagation();
+    });
   };
   reader.readAsDataURL(e.target.files[0]);
 };
-
-canvas.on("mouse:wheel", (opt) => {
-  var delta = opt.e.deltaY;
-  var zoom = canvas.getZoom();
-  zoom *= 0.999 ** delta;
-  if (zoom > 20) zoom = 20;
-  if (zoom < 0.01) zoom = 0.01;
-  canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
-  opt.e.preventDefault();
-  opt.e.stopPropagation();
-});
 
 canvas.requestRenderAll();
